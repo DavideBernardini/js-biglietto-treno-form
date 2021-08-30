38a1e7c13153d293a8ccc73ec24e5a70cac606eb
DavideBernardini <davidebernardini09@gmail.com>
2021-08-30 23:03:57 +0200

    Aggiunto js

--- js/script.js
+++ js/script.js
@@ -5,28 +5,60 @@
 // va applicato uno sconto del 40% per gli over 65.
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-// bottoni
-var btnGenera = document.getElementById("generate");
+// seleziono i bottoni
+var btnGenera = document.getElementById("genera");
+var btnAnnulla = document.getElementById("annulla");
 
-// evento bottone genera
-btnGenera.addEventListener("click",
-        function() {
-        // prendo i valori del form
-        var nameP = getElementById("name").value;
+// click bottone genera
+btnGenera.addEventListener('click', 
+    function() {
+        // prendo i valori dei campi input e select
+        var nome = document.getElementById("nome").value;
         var km = parseInt(document.getElementById("km").value);
-        var ageGroup = document.getElementById("age").value;
+        var fasciaEtà = document.getElementById("fascia-età").value;
 
-        // calcolo prezzo biglietto
-        var ticketPrice = km * 0.21;
-        if ( ageGroup == "minor" ) {
-            ticketPrice -= ticketPrice * 0.2;
-        } else if (ageGroup == "senior") {
-            ticketPrice -= ticketPrice * 0.4;
+        // genero una numero random per la carrozza e il codice CP
+        var carrozza = Math.floor((Math.random() * 9) + 1);
+        var cp = Math.floor((Math.random() * 10000) + 90000)
+
+        // calcolo il prezzo in base alla fascia di età
+        var prezzo = km * 0.21;
+        var offerta = "Biglietto standard";
+        if ( fasciaEtà == "minorenne" ) {
+            prezzo -= prezzo * 0.2;
+            offerta = "Sconto Young";
+        } else if (fasciaEtà == "over") {
+            prezzo -= prezzo * 0.4;
+            offerta = "Sconto Silver";
         } 
+        // Inserisco i dati nell'html
+        if ( nome == "" || !(isNaN(nome)) ) {
+            alert("Inserisci un nome e un cognome validi.")
+        } else if ( km == "" || isNaN(km) ) {
+            alert("Inserisci un valre numerico per indicare i Km da percorrere.")
+        } else {
+            document.getElementById("nome-passeggero").innerHTML = nome;
+            document.getElementById("tipo-offerta").innerHTML = offerta;
+            document.getElementById("carrozza").innerHTML = carrozza;
+            document.getElementById("codice-cp").innerHTML = cp;
+            document.getElementById("totale-prezzo").innerHTML = prezzo.toFixed(2) + "€";
+        }
+        
+        // rendo visibile il biglietto
+        document.getElementById("biglietto").classList.add("mostra");
     }
-    // inietto i dati nel biglietto
-    document.getElementById("passenger_name").innerHTML = nameP;
-    document.getElementById("price").innerHTML = ticketPrice;
-    // mostro il biglietto
-    document.getElementById("ticket_info").classList.add("show");
+);
+
+// click bottone annulla
+btnAnnulla.addEventListener('click', 
+    function() {
+        // nascondo il biglietto
+        document.getElementById("biglietto").classList.remove("mostra");
+
+        // resetto i campi del form
+        document.getElementById("nome").value = "";
+        document.getElementById("km").value = "";
+        document.getElementById("fascia-età").selectedIndex = "0";
+    }
+
 );
